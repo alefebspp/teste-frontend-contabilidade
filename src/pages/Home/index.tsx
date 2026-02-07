d76a2cc8f4a42e1616242d804b3792dd8e1b87fd
Alefe Bispo <alefe.bispo@onesight.com.br>
2026-02-06 22:07:19 -0300

    feat: gather all components into Home page component

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import ComparisonTable from 'components/ComparisonTable'
+import FactorsCarousel from 'components/FactorsCarousel'
+import FeaturesSection from 'components/FeaturesSection'
+import HeroSection from 'components/HeroSection'
+import PricingComparisonSection from 'components/PricingComparisonSection'
+
+const Home = () => (
+  <>
+    <HeroSection />
+    <PricingComparisonSection />
+    <FeaturesSection />
+    <ComparisonTable />
+    <FactorsCarousel />
+  </>
+)
+
+export default Home
